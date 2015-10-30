d8e7f3a9848055f02b34133c8d0820e8bfa1b4f5
Jean-Louis Mbaka <jeanlouis.mbaka@gmail.com>
2015-10-30 13:58:35 +0100

    [7.3]Denied access to new post page when not logged in.

--- client/views/posts/post_submit.js
+++ client/views/posts/post_submit.js
@@ -6,7 +6,7 @@ Template.postSubmit.events({
             url: $(e.target).find('[name=url]').val(),
             title: $(e.target).find('[name=title]').val(),
             message: $(e.target).find('[name=message]').val()
-        }
+        };
 
         post._id = Posts.insert(post);
         Router.go('postPage', post);
