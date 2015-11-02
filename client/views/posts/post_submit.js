92ae5cb51d17b72eb62585b739c5d04ea9ab7b16
Jean-Louis Mbaka <jeanlouis.mbaka@gmail.com>
2015-11-02 10:42:26 +0100

    [FIX]Use the same code as Github repo

--- client/views/posts/post_submit.js
+++ client/views/posts/post_submit.js
@@ -4,14 +4,19 @@ Template.postSubmit.events({
 
         var post = {
             url: $(e.target).find('[name=url]').val(),
-            title: $(e.target).find('[name=title]').val(),
-            message: $(e.target).find('[name=message]').val()
+            title: $(e.target).find('[name=title]').val()
         };
 
-        Meteor.call('postInsert', post, function (error, id) {
+        Meteor.call('postInsert', post, function (error, result) {
+            // display the error to the user and abort
             if (error)
                 return alert(error.reason);
-            Router.go('postPage', {_id: id});
+
+            // show this result but route anyway
+            if (result.postExists)
+                alert("This link has already been posted");
+
+            Router.go('postPage', {_id: result._id});
         });
     }
 });
