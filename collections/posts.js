8ae83e8943064f581c97be1d0c1550d406b4695b
Jean-Louis Mbaka <jeanlouis.mbaka@gmail.com>
2015-11-04 00:05:18 +0100

    [8-2]Added basic permission to check the post's owner.

--- collections/posts.js
+++ collections/posts.js
@@ -1,5 +1,10 @@
 Posts = new Meteor.Collection('posts');
 
+Posts.allow({
+    update: ownsDocument,
+    remove: ownsDocument
+});
+
 Meteor.methods({
     postInsert: function(postAttributes) {
         check(this.userId, String);
